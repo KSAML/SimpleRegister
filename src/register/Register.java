004edf4d58e5202e6c6c215b0abf85956d4dd2a1
samuel <samuel@WEBDEV02.lumjo.net>
2022-03-10 11:35:29 +0300

    This is a simple registration project

---
+++ src/register/Register.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package register;
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+
+public class Register {
+public class SimpleRegister extends Applet implements ActionListener{
+    String name=""; String Comments="", Gender="";
+    TextField nm=new TextField(10);
+    TextField Comms=new TextField();
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+    public static void main(String[] args) {
+    
+    }
+    
+}
